6ca438ac306f6fd5476933dcd6485bc45ca0ff23
Siavash Mir arabbaygi <smirarab@login4.ranger.tacc.utexas.edu>
2012-06-13 10:15:23 -0500

    Lots of improvements to reroot script

--- src/mirphyl/utils/reroot.py
+++ src/mirphyl/utils/reroot.py
@@ -13,13 +13,15 @@ if __name__ == '__main__':
     
     treeName = sys.argv[1]
 
-    outgroups = sys.argv[2].split(",")    
+    outgroups = sys.argv[2].split(",")
+    
+    use_mrca = True if len(sys.argv) > 3 and sys.argv[3] == "-mrca" else False
     #cmd = 'find %s -name "%s" -print' % (treeDir,treeName)
     #print cmd
     #for file in os.popen(cmd).readlines():     # run find command        
     #    name = file[:-1]                       # strip '\n'                
     #    fragmentsFile=name.replace(treeName,"sequence_data/short.alignment");
-    resultsFile="%s.rooted" % treeName
+    resultsFile="%s.rooted"%treeName[:-9] if treeName.endswith("unrooted") else "%s.rooted" % treeName
     print "writing results to %s" %resultsFile
     trees = dendropy.TreeList.get_from_path(treeName, 'newick',rooted=True)
     for tree in trees:
@@ -31,16 +33,29 @@ if __name__ == '__main__':
                 n = tree.find_node_with_taxon_label(out)
                 if n is None:
                     continue            
-                outns.append(n.taxon)           
+                outns.append(n.taxon)  
             if len (outns) != 0:
                 mrca = tree.mrca(taxa=outns)
+		#if not mono-phyletic, then use the first
+		print len(outns)
+		print use_mrca
+		print len (mrca.leaf_nodes())
+		if not use_mrca and len (mrca.leaf_nodes()) == len(outns):
+		    print "selected set is not mono-phyletic. Using %s " %outns[0]
+		    mrca = tree.find_node_with_taxon_label(outs[0])
                 break                    
-            
         if mrca is None:
-            raise KeyError("Outgroups not found: %s" %outgroups)        
-        print "rerooting on %s" % [s.label for s in outns]
+            raise KeyError("Outgroups not found: %s" %outgroups)
+	
+ 
+	if mrca.parent_node is None:
+	    print ("Already rooted at the root.")
+        #print "rerooting on %s" % [s.label for s in outns]
         #tree.reroot_at_midpoint()
-        tree.reroot_at_edge(mrca.edge, update_splits=False,length1=mrca.edge.length/2,length2=mrca.edge.length/2)        
+	elif mrca.edge.length is not None:
+	        tree.reroot_at_edge(mrca.edge, update_splits=False,length1=mrca.edge.length/2,length2=mrca.edge.length/2)        
+	else:
+		tree.reroot_at_edge(mrca.edge, update_splits=False)
         #tree.reroot_at_midpoint(update_splits=False)
         
     trees.write(open(resultsFile,'w'),'newick',edge_lengths=True, internal_labels=False)
