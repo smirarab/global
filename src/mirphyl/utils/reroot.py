e767cf4a6721937e069e9269729b2fbcd5928ec9
Siavash Mir arabbaygi <smirarab@login4.ranger.tacc.utexas.edu>
2012-06-13 10:30:20 -0500

    Added documentation for reroot.py

--- src/mirphyl/utils/reroot.py
+++ src/mirphyl/utils/reroot.py
@@ -8,9 +8,39 @@ import dendropy
 import sys
 import os
 import copy
-
+''' roots the tree on the given outgroups. Input is:
+-- first argument: a path to the tree file
+-- second argument: a list of outgroups, separated by camma. 
+-- third argument: (optional) -mrca
+The script goes through the list of outgroups. 
+If the outgroup is found in the tree, the tree is rooted at that outgroup.
+Otherwise, the next outgroup in the list is used. 
+Each element in the camma-delimited list is itself a + delimited list of taxa.
+By default the script makes sure that this list of taxa are mono-phyletic
+in the tree (if not, an error is generated) and rootss the tree at the parent
+of these taxa.
+Using -mrca this mono-phyletic requirement can be overwritten. 
+'''
 if __name__ == '__main__':
     
+    if len (sys.argv) < 2:
+	print '''USAGE: %s [tree_file] [outgroups] [-mrca (optional)]\n
+-- tree_file: a path to the newick tree file\n
+-- outgroups: a list of outgroups, separated by camma.
+The script goes through the list of outgroups. 
+If the outgroup is found in the tree, the tree is rooted at that outgroup.
+Otherwise, the next outgroup in the list is used. 
+Each element in the camma-delimited list is itself a + delimited list of taxa.
+By default the script makes sure that this list of taxa are mono-phyletic
+in the tree (if not, an error is generated) and roots the tree at the parent
+of the outgroups given in the + delimited list.\n
+Example: HUMAN,ANOCA,STRCA+TINMA first tries to root at HUMAN, if not present, 
+tries to use ANOCA, if not present, tries to root at parent of STRCA and TINMA
+which need to be mono-phyletic.\n
+-- (optional) -mrca: using this option the mono-phyletic
+requirement is relaxed.
+''' %sys.argv[0]
+        sys.exit(1)
     treeName = sys.argv[1]
 
     outgroups = sys.argv[2].split(",")
