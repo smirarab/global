58e717e65034efb0beaf9484512e8398e0db914b
Siavash Mirarab <smirarab@gmail.com>
2012-07-16 11:50:38 -0500

    Misc.

--- src/mirphyl/utils/reroot.py
+++ src/mirphyl/utils/reroot.py
@@ -10,35 +10,36 @@ import os
 import copy
 ''' roots the tree on the given outgroups. Input is:
 -- first argument: a path to the tree file
--- second argument: a list of outgroups, separated by camma. 
+-- second argument: a list of outgroups, separated by comma. 
 -- third argument: (optional) -mrca
 The script goes through the list of outgroups. 
 If the outgroup is found in the tree, the tree is rooted at that outgroup.
 Otherwise, the next outgroup in the list is used. 
 Each element in the camma-delimited list is itself a + delimited list of taxa.
-By default the script makes sure that the set of outgroups used are mono-phyletic
-in the tree and rootss the tree at the node leading to the clade represented by outgroups.
-If outgroups are not mono-phyletic, the first one is used.
-Using -mrca this mono-phyletic requirement can be relaxed, so that MRCA is always used. 
+By default the script makes sure that the set of outgroups used are monophyletic
+in the tree and roots the tree at the node leading to the clade represented by outgroups.
+If outgroups are not monophyletic, the first one is used.
+Using -mrca this monophyletic requirement can be relaxed, so that MRCA is always used. 
 '''
 if __name__ == '__main__':
     
     if len (sys.argv) < 2:
-	print '''USAGE: %s [tree_file] [outgroups] [-mrca (optional)]
+        print '''USAGE: %s [tree_file] [outgroups] [-mrca (optional)]
 
 -- tree_file: a path to the newick tree file
 
--- outgroups: a list of outgroups, separated by camma.
+-- outgroups: a list of outgroups, separated by comma.
 The script goes through the list of outgroups. If the outgroup is found in the tree, 
 the tree is rooted at that outgroup. Otherwise, the next outgroup in the list is used. 
-Each element in the camma-delimited list is itself a + delimited list of taxa.
-By default the script makes sure that this list of taxa are mono-phyletic
+Each element in the comma-delimited list is itself a + delimited list of taxa.
+By default the script makes sure that this list of taxa are monophyletic
 in the tree and roots the tree at the node leading to the clade represented 
 by outgroups given in the + delimited list.
+Alternatively, you can specify -m which will result in mid-point rooting.
 
 Example: HUMAN,ANOCA,STRCA+TINMA first tries to root at HUMAN, if not present, 
 tries to use ANOCA, if not present, tries to root at parent of STRCA and TINMA
-which need to be mono-phyletic. If not mono-phyletic, roots at STRCA.
+which need to be monophyletic. If not monophyletic, roots at STRCA.
 
 -- (optional) -mrca: using this option the mono-phyletic requirement is relaxed 
 and always the mrca of the + delimited list of outgroups is used.
@@ -55,47 +56,56 @@ and always the mrca of the + delimited list of outgroups is used.
     #    name = file[:-1]                       # strip '\n'                
     #    fragmentsFile=name.replace(treeName,"sequence_data/short.alignment");
     resultsFile="%s.rooted"%treeName[:-9] if treeName.endswith("unrooted") else "%s.rooted" % treeName
-    print "writing results to %s" %resultsFile
+    print "Reading input trees ..."
     trees = dendropy.TreeList.get_from_path(treeName, 'newick',rooted=True)
-    i = 0;
-    for tree in trees:
+    i = 0;    
+    for tree in trees:        
         i+=1
-        print i
-        mrca = None
-        for outgroup in outgroups:  
-            outs = outgroup.split("+")
-            outns = []                    
-            for out in outs:          
-                n = tree.find_node_with_taxon_label(out)
-                if n is None:
-                    continue            
-                outns.append(n.taxon)  
-            if len (outns) != 0:
-                # Find an ingroup and root the tree there
-                for n in tree.leaf_iter():
-                    if n.taxon not in outns:
-                        ingroup=n
-                        break
-                # print "rerooting at ingroup %s" %ingroup.taxon.label
-                tree.reroot_at_edge(ingroup.edge, update_splits=True,length1=ingroup.edge.length/2,length2=ingroup.edge.length/2)
-                mrca = tree.mrca(taxa=outns)
-		#if not mono-phyletic, then use the first
-		if not use_mrca and len (mrca.leaf_nodes()) != len(outns):
-		    print "selected set is not mono-phyletic. Using %s instead. " %outns[0]
-		    mrca = tree.find_node_with_taxon_label(outs[0])
+        oldroot = tree.seed_node
+        print "Tree %d:" %i
+        if outgroups[0] == "-m":            
+            print "Midpoint rooting ... "
+            tree.reroot_at_midpoint(update_splits=False)
+        else:             
+            mrca = None
+            for outgroup in outgroups:  
+                outs = outgroup.split("+")
+                outns = []                    
+                for out in outs:          
+                    n = tree.find_node_with_taxon_label(out)
+                    if n is None:
+                        continue            
+                    outns.append(n.taxon)  
+                if len (outns) != 0:
+                    # Find an ingroup and root the tree there
+                    for n in tree.leaf_iter():
+                        if n.taxon not in outns:
+                            ingroup=n
+                            break
+                    # print "rerooting at ingroup %s" %ingroup.taxon.label
+                    '''reroot at an ingroup, so that outgroups form monophyletic groups, if possible'''
+                    tree.reroot_at_edge(ingroup.edge, update_splits=True,length1=ingroup.edge.length/2,length2=ingroup.edge.length/2)
+                    mrca = tree.mrca(taxa=outns)
+            #if not mono-phyletic, then use the first
+            if not use_mrca and len (mrca.leaf_nodes()) != len(outns):
+                print "selected set is not mono-phyletic. Using %s instead. " %outns[0]
+                mrca = tree.find_node_with_taxon_label(outs[0])
                 break                    
-        if mrca is None:
-            raise KeyError("Outgroups not found: %s" %outgroups)
-	
- 
-	if mrca.parent_node is None:
-	    print ("Already rooted at the root.")
-        #print "rerooting on %s" % [s.label for s in outns]
-        #tree.reroot_at_midpoint()
-	elif mrca.edge.length is not None:
-	        tree.reroot_at_edge(mrca.edge, update_splits=False,length1=mrca.edge.length/2,length2=mrca.edge.length/2)        
-	else:
-		tree.reroot_at_edge(mrca.edge, update_splits=False)
-        #tree.reroot_at_midpoint(update_splits=False)
-        
-    trees.write(open(resultsFile,'w'),'newick',edge_lengths=True, internal_labels=False)
+            if mrca is None:
+                raise KeyError("Outgroups not found: %s" %outgroups)
+            if mrca.parent_node is None:
+                print "Already rooted at the root."
+                #print "rerooting on %s" % [s.label for s in outns]
+                #tree.reroot_at_midpoint()
+            elif mrca.edge.length is not None:
+                tree.reroot_at_edge(mrca.edge, update_splits=False,length1=mrca.edge.length/2,length2=mrca.edge.length/2)        
+            else:
+                tree.reroot_at_edge(mrca.edge, update_splits=False)
+        '''This is to fix internal node labels when treated as support values''' 
+        while oldroot.parent_node != tree.seed_node:
+            oldroot.label = oldroot.parent_node.label
+            oldroot = oldroot.parent_node
+        oldroot.label = oldroot.sister_nodes()[0].label    
+            #tree.reroot_at_midpoint(update_splits=False)            
+    print "writing results to %s" %resultsFile        
+    trees.write(open(resultsFile,'w'),'newick',edge_lengths=True, internal_labels=True)
---
+++ src/perl/calculate_bootstrap_tree_from_bipartitions.pl
@@ -0,0 +1,91 @@
+#!/usr/bin/env perl
+
+# calculate bootstrap tree from bipartitions output
+
+# pick up global settings
+use FindBin qw($Bin);
+use lib "$Bin/../global";
+use setenv;
+use framework;
+# also make environment changes
+setenv::setenv();
+
+use Getopt::Std;
+use Cwd;
+
+# for sanity's sake
+use strict;
+
+# for hi res timing - from Li-San
+use Time::HiRes qw(gettimeofday);
+
+# for temp files
+use File::Temp qw/ tempfile tempdir /;
+
+sub process {
+    my $infile = shift;
+    my $outfile = shift;
+    my $threshold = shift;
+
+    my $scriptdir = getcwd;
+
+    if (!(-e $infile) || (-z $infile)) {
+	die "ERROR: $infile doesn't exist! Exiting.\n";
+    }
+
+    chdir (setenv::SINDHU_CODE_DIR);
+    framework::systemSafe(setenv::JAVA_COMMAND . " com.dcm.newick.BootstrapTree -i $infile -o $outfile -t $threshold");
+    chdir ($scriptdir);
+
+    # postprocess
+    my $treestring = framework::getStringFromFile($outfile);
+
+    $treestring =~ s/\)[0-9]+/\)/g;
+    $treestring =~ s/\)null/\)/g;
+
+    open (OUT, ">$outfile");
+    print OUT "$treestring\n";
+    close (OUT);
+}
+
+sub printUsage {
+    print STDERR
+	"Usage: perl calculate_bootstrap_tree_from_bipartitions.pl \n" .
+	"       -i <input RAxML bipartitions file with FULL PATH> REQUIRED \n" .
+	"       -o <output bootstrap tree with FULL PATH> REQUIRED \n" .
+	"       -t <bootstrap threshold in [0-100]> \n"
+;
+    exit 1;
+}
+
+# output all stats to STATS file directly
+sub main {
+    # for now, just pass in 
+    # raw sequences file (with path)
+    getopt("iot");
+
+    if (($Getopt::Std::opt_i eq "") ||
+	($Getopt::Std::opt_o eq "") ||
+	($Getopt::Std::opt_t eq "") 
+	) {	
+	printUsage;
+    }
+
+    my $currdir = getcwd;
+
+    $Getopt::Std::opt_i =~ s/CURRDIR\_MARKER/$currdir/g;
+    $Getopt::Std::opt_o =~ s/CURRDIR\_MARKER/$currdir/g;
+
+    print "Processing $Getopt::Std::opt_i...";
+
+    # use $workdir instead of $Getopt::Std::opt_w
+    process($Getopt::Std::opt_i,
+	    $Getopt::Std::opt_o,
+	    $Getopt::Std::opt_t
+	    );
+
+    print "done.\n";
+}
+
+main;
+
