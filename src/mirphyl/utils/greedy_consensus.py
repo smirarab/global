bf71637f13eb70b5e4a7f12bee4a13cf40916f23
Siavash Mirarab <smirarab@gmail.com>
2013-07-11 18:03:50 -0500

    MRP works now too

--- src/mirphyl/utils/greedy_consensus.py
+++ src/mirphyl/utils/greedy_consensus.py
@@ -12,15 +12,17 @@ import copy
 if __name__ == '__main__':
 
     if ("--help" in sys.argv) or ("-?" in sys.argv) or ("-h" in sys.argv) or len(sys.argv) < 4:
-        sys.stderr.write("usage: %s [<trees-file-path>] [threshold] [<out-file-path>]\n"%sys.argv[0])
+        sys.stderr.write("usage: %s [threshold] [<out-file-path>] [<trees-file-paths>] \n"%sys.argv[0])
         sys.exit(1)
     
-    treeName = sys.argv[1]
-    threshold = float(sys.argv[2])
-    resultsFile = sys.argv[3]
+    threshold = float(sys.argv[1])
+    resultsFile = sys.argv[2]
 
+    trees = dendropy.TreeList()
    
-    trees = dendropy.TreeList.get_from_path(treeName, 'newick')
+    for fpath in sys.argv[3:]:
+        trees.read_from_path(fpath, 'newick')
+
     con_tree = trees.consensus(min_freq=threshold)     
         #tree.reroot_at_edge(n.edge, update_splits=False)
         #tree.reroot_at_midpoint(update_splits=False)
