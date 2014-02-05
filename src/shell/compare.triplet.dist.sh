c21cadb4deefa680beb732eb128783b37967a541
Siavash Mirarab <smirarab@gmail.com>
2014-02-05 14:31:39 -0600

    misc. small changes

--- src/shell/compare.triplet.dist.sh
+++ src/shell/compare.triplet.dist.sh
@@ -7,17 +7,18 @@ shift
 
 while (( "$#" )); do
    
-  echo -n $1" "
   #cat $ref | paste - $1 |awk 'BEGIN{s=0;l=0}{l+=1;s+=sqrt(($2-$6)^2+($3-$7)^2+($4-$8)^2)}END{print s/l}'
   cat $ref | paste - $1 |awk '
    BEGIN{s=0;l=0}
    {  l+=1;
       y=($2+$3+$4)
       x=($8+$6+$7);
-      a=(sqrt($2/x)-sqrt($6/y))^2 + (sqrt($3/x)-sqrt($7/y))^2 + (sqrt($4/x)-sqrt($8/y))^2;
-      s+=sqrt(a)/sqrt(2);
+      a=(sqrt($2/y)-sqrt($6/x))^2 + (sqrt($3/y)-sqrt($7/x))^2 + (sqrt($4/y)-sqrt($8/x))^2;
+      d=sqrt(a)/sqrt(2);
+      s+=d;
+      print $1,d;
    }
-   END{print s/l}'
+   END{print "'$1'",s/l}'
 
   shift
 
