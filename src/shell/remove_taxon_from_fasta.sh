1e66f861e92ce654c8c1555ddd1cbd3feab18552
Siavash Mirarab <toranj>
2015-08-29 12:40:53 -0700

    update trim and remove

--- src/shell/remove_taxon_from_fasta.sh
+++ src/shell/remove_taxon_from_fasta.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+
 if [ $# -lt 2 ]; then
 	echo "USAGE: $0 <sequence name> <input file> [-rev]"
 	exit 1
 fi
+
 if [ "$3" == "-rev" ]; then
   c='/>/ {p=0} />('$1')$/ {p=1} {if (p) {print $0};}'
 else
   c='/>/ {p=1} />('$1')$/ {p=0} {if (p) {print $0};}'
 fi
+
 awk "$c" $2
--- src/shell/trim-fasta.sh
+++ src/shell/trim-fasta.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+test $# == 2 || { echo USAGE: $0 desired_length fasta_file; exit 1; }
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+source $DIR/setup.sh
+
 size=$1
 shift
 
-simplifyfasta.sh $1 |awk '/^ *[^>]/{print substr($1,0,'$size')} /^ *[>]/{print $1}'
+$DIR/simplifyfasta.sh $1 |awk '/^ *[^>]/{print substr($1,0,'$size')} /^ *[>]/{print $1}'
