2c41c6ad9a892013ab9938c8b2ca3d0c4a825d60
esayyari <Erfan@Erfans-MacBook-Pro.local>
2015-08-31 13:01:33 -0700

    adding quartet.freq.sh

--- src/shell/triplet.freq.sh
+++ src/shell/triplet.freq.sh
@@ -7,9 +7,9 @@ source $DIR/setup.sh
 
 tmp=`mktemp`
 
-for `cat $1`; do 
-  echo -n "@" >$tmp; 
-  $WS_GLB_BIN/triplets.soda2103 fancy printTriplets $tmp; 
+for x in `cat $1`; do 
+  echo -n "$x" >$tmp; 
+  $WS_GLB_BIN/trip_bin fancy printTriplets $tmp; 
 done |python $WS_GLB_PUTIL/summarize.triplets.py
 
 #cat $1| xargs -I@ sh -c 'echo -n "@" >'$tmp'; '$WS_GLB_BIN'/triplets.soda2103 fancy printTriplets '$tmp';'|python $WS_GLB_PUTIL/summarize.triplets.py
