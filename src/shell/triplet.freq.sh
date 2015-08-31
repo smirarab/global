ea607d1824c79712b2dc21e0e18638e3dc8bee56
Siavash Mirarab <smirarab@gmail.com>
2015-08-31 11:48:56 -0700

    avoid xargs; confusing mac

--- src/shell/triplet.freq.sh
+++ src/shell/triplet.freq.sh
@@ -6,6 +6,12 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source $DIR/setup.sh
 
 tmp=`mktemp`
-cat $1| xargs -I@ sh -c 'echo -n "@" >'$tmp'; '$WS_GLB_BIN'/triplets.soda2103 fancy printTriplets '$tmp';'|python $WS_GLB_PUTIL/summarize.triplets.py
+
+for `cat $1`; do 
+  echo -n "@" >$tmp; 
+  $WS_GLB_BIN/triplets.soda2103 fancy printTriplets $tmp; 
+done |python $WS_GLB_PUTIL/summarize.triplets.py
+
+#cat $1| xargs -I@ sh -c 'echo -n "@" >'$tmp'; '$WS_GLB_BIN'/triplets.soda2103 fancy printTriplets '$tmp';'|python $WS_GLB_PUTIL/summarize.triplets.py
 
 rm $tmp
