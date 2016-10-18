c2c5a02042f9e1aa1cea79f30c4ffa2a21e25bcb
Siavash Mirarab <smirarab@gmail.com>
2016-10-18 15:21:20 -0700

    update distance

--- src/shell/distance.sh
+++ src/shell/distance.sh
@@ -1,9 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
-test $# == 2  || { echo USAGE: input_fasta oupt_stat_file_name; exit 1 }
+test $# == 2  || { echo USAGE: input_fasta oupt_stat_file_name; exit 1; }
 
 output=$2
 
 java -cp $WS_HOME/global/src/java/distance AlignmentStatistics $*
 
-sed -i -e 's/ |//g' $output
+sed -i -e "s/ |//g" $output
+
+
